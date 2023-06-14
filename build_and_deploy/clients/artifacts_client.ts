ef893e878233766399b26dcfd732b98824704c2c
HarshaMaddipudi <harsha.maddipudi@kroger.com>
2023-06-14 11:19:10 -0400

    change throw error to system logger info

--- build_and_deploy/clients/artifacts_client.ts
+++ build_and_deploy/clients/artifacts_client.ts
@@ -509,7 +509,9 @@ export class ArtifactClient {
                 if(body != null && response.error != null && response.error.message != null) {
                     msg = response.error.message;
                 }
-                throw new Error(`Checkstatus => status: ${resStatus}; status message: ${msg}`);            }
+                // throw new Error(`Checkstatus => status: ${resStatus}; status message: ${msg}`);
+                SystemLogger.info(`Checkstatus => status: ${resStatus}; status message: ${msg}`);           
+            }
 
             if (!body) {
                 await this.delay(delayMilliSecs);
