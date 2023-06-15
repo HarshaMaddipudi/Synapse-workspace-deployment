0e4e6912aa5f3a7b3e98ebdf7db3a72dcdde9c60
HarshaMaddipudi <102694253+HarshaMaddipudi@users.noreply.github.com>
2023-06-15 09:36:38 -0400

    status check error handling change (#1)

--- build_and_deploy/clients/artifacts_client.ts
+++ build_and_deploy/clients/artifacts_client.ts
@@ -509,7 +509,8 @@ export class ArtifactClient {
                 if(body != null && response.error != null && response.error.message != null) {
                     msg = response.error.message;
                 }
-                throw new Error(`Checkstatus => status: ${resStatus}; status message: ${msg}`);          
+                // throw new Error(`Checkstatus => status: ${resStatus}; status message: ${msg}`);
+                SystemLogger.info(`Checkstatus => status: ${resStatus}; status message: ${msg}`);       
             }
 
             if (!body) {
